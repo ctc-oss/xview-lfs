d278ba7ad8ac00f501ba3c0c621b1c32431e6f28
John Wass <wassj@ctc.com>
2020-01-29 10:22:08 -0500

    find subpackages

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 requires = []
 with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
@@ -7,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
 
 setup(name='xview_lfs',
       version='0.1.0',
-      packages=['xview_lfs'],
+      packages=find_packages(),
       install_requires=requires,
       include_package_data=True
       )
