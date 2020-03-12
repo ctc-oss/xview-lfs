b9fd9d62d9339f9d297e0dedac05910dfde753a7
John Wass <wassj@ctc.com>
2020-03-11 22:55:17 -0400

    v0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
     requires = f.read().splitlines()
 
 setup(name='xview_lfs',
-      version='0.1.0',
+      version='0.3.0',
       packages=find_packages(),
       install_requires=requires,
       include_package_data=True
